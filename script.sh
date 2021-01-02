ba52ebdfdf42f342f3e3cf0b5c87666fbaeffe22
Domingo Ruiz Arroyo <ordenadordomi@gmail.com>
2021-01-02 17:32:17 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 while true; do
     echo "[$(date)] The backup starts"
-    lftp -e "mirror -R /mnt/ ${DESTINATION}" -u ${USER},${PASSWORD} ftp://${SERVER}
-	echo "[$(date)] The backup finish. The next one will take place in 24 hours"
+    pkill lftp
+    lftp -e "mirror -R /mnt/ ${DESTINATION}" -u ${USER},${PASSWORD} ftp://${SERVER} &
+    echo "[$(date)] The backup finish. The next one will take place in 24 hours"
     sleep 24h
 done
